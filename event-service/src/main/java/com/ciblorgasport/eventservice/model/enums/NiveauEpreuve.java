6c120a7248b4bad9d99468cfa138fdf9ac06137c
ikram leila hamzaoui <128831018+ikramleilahm@users.noreply.github.com>
2026-03-09 13:23:27 +0100

    feat: modif du niveau epreuve pour les adapter selon nos disciplines

--- event-service/src/main/java/com/ciblorgasport/eventservice/model/enums/NiveauEpreuve.java
+++ event-service/src/main/java/com/ciblorgasport/eventservice/model/enums/NiveauEpreuve.java
@@ -1,8 +1,10 @@
 package com.ciblorgasport.eventservice.model.enums;
 
 public enum NiveauEpreuve {
-    QUALIFICATION,
-    QUART_DE_FINALE,
-    DEMI_FINALE,
-    FINALE
+    PHASE_PRELIMINAIRE,   // Couvre : SERIES, PRELIMINAIRES, PROGRAMME_TECHNIQUE
+    PHASE_GROUPE,         // Couvre : POULE (water-polo)
+    QUART_DE_FINALE,      // Water-polo uniquement
+    DEMI_FINALE,          // Natation, Water-polo, Plongeon
+    PETITE_FINALE,        // Match pour 3e place (water-polo)
+    FINALE                // Toutes disciplines
 }
